881d20ebb768af7ad5e49bbd7118cff1ef3aae22
ilyass <ilyass.df.01@gmail.com>
2024-01-24 01:24:15 -0800

    a

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -27,32 +27,32 @@ class Square:
     def size(self, value):
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-		if value < 0:
-			raise ValueError("size must be >= 0")
+	if value < 0:
+	    raise ValueError("size must be >= 0")
         self.__size = value
 
-	def area(self)
-		"""Area of this square.
+    def area(self)
+	"""Area of this square.
 
-		Returns:
-			The size squared.
-		"""
-		return self.__size ** 2
+	Returns:
+	    The size squared.
+	"""
+	return self.__size ** 2
 
-	def __eq__(self, other):
-		return self.area() == other.area()
+    def __eq__(self, other):
+	return self.area() == other.area()
 
-	def __ne__(self, other):
-		return self.area() != other.area()
+    def __ne__(self, other):
+	return self.area() != other.area()
 
-	def __gt__(self, other):
-		return self.area() > other.area()
+    def __gt__(self, other):
+	return self.area() > other.area()
 
-	def __ge__(self, other):
-		return self.area() >= other.area()
+    def __ge__(self, other):
+	return self.area() >= other.area()
 
-	def __lt__(self, other):
-		return self.area() < other.area()
+    def __lt__(self, other):
+	return self.area() < other.area()
 
-	def __le__(self, other):
-		return self.area() <= other.area()
+    def __le__(self, other):
+	return self.area() <= other.area()
