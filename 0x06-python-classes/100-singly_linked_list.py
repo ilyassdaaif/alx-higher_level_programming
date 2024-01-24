55cec5716e977cb806ba49fe3e55ff98e12ce907
ilyass <ilyass.df.01@gmail.com>
2024-01-24 00:57:02 -0800

    a

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -21,7 +21,7 @@ class Node:
 
     @data.setter
     def data(self, value):
-        if (not isinstance(value, int):
+        if not isinstance(value, int):
              raise TypeError("data must be an integer")
         self.__data = value
 
