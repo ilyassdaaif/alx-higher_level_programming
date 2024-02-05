ee343d046f9a12665fa2ef8dac2e7e3613e755fd
ilyass <ilyass.df.01@gmail.com>
2024-02-05 12:26:27 -0800

    a

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,7 +3,7 @@
 
 
 class MyList(list):
-    '''Custom MyList class.'''.
+    '''Custom MyList class.'''
     def print_sorted(self):
         '''Method for printing sorted list.'''
         print(sorted(self))
