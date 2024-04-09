8a042999bf144c9bf4a1c6c66f3b51bca85ec3c4
ilyass <ilyass.df.01@gmail.com>
2024-04-09 15:18:45 -0700

    a

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -7,11 +7,11 @@ class Rectangle {
     }
   }
 
-print () {
+  print () {
     for (let i = 0; i < this.height; i++) {
       let s = '';
       for (let j = 0; j < this.width; j++) {
-	s += 'X';
+        s += 'X';
       }
       console.log(s);
     }
