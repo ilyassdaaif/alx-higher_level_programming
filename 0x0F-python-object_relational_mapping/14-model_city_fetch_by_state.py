322a2e834d353547934bfe83e6e5b1fab84e57ee
ilyass <ilyass.df.01@gmail.com>
2024-04-14 19:47:05 -0700

    a

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,7 +8,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sys import argv
 
-if __name__ = "__main__":
+if __name__ == "__main__":
     # create an engine
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
         argv[1], argv[2], argv[3]), pool_pre_ping=True)
