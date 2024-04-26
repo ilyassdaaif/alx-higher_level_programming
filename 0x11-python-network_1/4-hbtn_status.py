8e7c73c4e38e23fbaf0938e8f8728adaf66bbe09
ilyass <ilyass.df.01@gmail.com>
2024-04-26 16:29:04 -0700

    a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,10 +1,20 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
 import requests
 
-if __name__ == "__main__":
-    r = requests.get("https://intranet.hbtn.io/status")
+def fetch_status():
+    # URL to fetch status from
+    url = "https://alx-intranet.hbtn.io/status"
+    
+    # Make a GET request to the URL
+    response = requests.get(url)
+    
+    # Get the content of the response and ensure it is of type 'str'
+    content = response.text
+    
+    # Print the formatted output
     print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
-    print("\t- status code: {}".format(r.status_code))
+    print("\t- type: {}".format(type(content)))
+    print("\t- content: {}".format(content))
+
+if __name__ == "__main__":
+    fetch_status()
