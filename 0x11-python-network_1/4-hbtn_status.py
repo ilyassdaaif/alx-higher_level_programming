02f1bbf5ab3f5602ec5e443283c7174ee1b614b0
ilyass <ilyass.df.01@gmail.com>
2024-04-26 16:36:25 -0700

    a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,10 +5,13 @@ def fetch_status():
     url = "https://alx-intranet.hbtn.io/status"
     try:
         response = requests.get(url)
-        content = response.text
-        print("Body response:$")
-        print("\t- type: {}$".format(type(content)))
-        print("\t- content: {}$")
+        if response.status_code == 200:
+            content = response.text
+            print("Body response:$")
+            print("\t- type: {}$".format(type(content)))
+            print("\t- content: {}$".format(content))
+        else:
+            print(f"Failed to fetch data: HTTP {response.status_code}$")
     except requests.RequestException as e:
         print(f"Error fetching the URL: {e}$")
 
