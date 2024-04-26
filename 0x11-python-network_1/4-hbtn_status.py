4fdfa9c3f448569d8e1acb9619f8805626e28dd4
ilyass <ilyass.df.01@gmail.com>
2024-04-26 15:29:08 -0700

    a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -2,9 +2,9 @@
 """Fetches https://intranet.hbtn.io/status."""
 import requests
 
-
 if __name__ == "__main__":
     r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
     print("\t- type: {}".format(type(r.text)))
     print("\t- content: {}".format(r.text))
+    print("\t- status code: {}".format(r.status_code))
