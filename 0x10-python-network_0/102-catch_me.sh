9f8b59e947d08a9df463e0d9e2a5eb762a06955e
ilyass <ilyass.df.01@gmail.com>
2024-04-26 14:52:13 -0700

    a

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -2,4 +2,4 @@
 # Script that makes a request to 0.0.0.0:5000/catch_me causing the server to respond with "You got me!"
 
 # Make a request to the server using curl
-curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool" -L -o /dev/null -w "You got me!"
+curl -s -X PUT 0.0.0.0:5000/catch_me -d "user_id=98" -H "Origin: HolbertonSchool" -o /dev/null
