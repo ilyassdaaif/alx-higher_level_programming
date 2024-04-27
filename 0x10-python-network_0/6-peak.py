befd604872ffa8b54cf662923e3db7494bc82045
ilyass <ilyass.df.01@gmail.com>
2024-04-27 13:30:20 -0700

    a

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,24 +1,20 @@
-#!/usr/bin/python3
-"""script for finding peak in list of ints, interview prep
-"""
-
-"""
-    THOUGHT PROCESS
-        it is not sorted, so sorting would take n(log(n))
-            -> not worth sorting
-        looping through and keeping track of max (brute force)
-            -> O(n)
-
-        possibly looping from each end reducing to 1/2 run time
-            -> still O(n)
-"""
-
-
 def find_peak(list_of_integers):
-    """BRUTE force implementation for question
-    """
-    max_i = None
-    for ele in list_of_integers:
-        if max_i is None or max_i < ele:
-            max_i = ele
-    return max_i
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low <= high:
+        mid = (low + high) // 2
+        # Check if current element is a peak
+        if (mid == 0 or list_of_integers[mid - 1] <= list_of_integers[mid]) and \
+           (mid == len(list_of_integers) - 1 or list_of_integers[mid + 1] <= list_of_integers[mid]):
+            return list_of_integers[mid]
+        # If the left neighbor is greater, move left
+        elif mid > 0 and list_of_integers[mid - 1] > list_of_integers[mid]:
+            high = mid - 1
+        # If the right neighbor is greater, move right
+        else:
+            low = mid + 1
+
+    return None  # This line should not be reached
