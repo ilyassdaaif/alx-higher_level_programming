81b0ee2191d3924585c2a09ddcff9831a9422d33
ilyass <ilyass.df.01@gmail.com>
2024-04-27 14:05:15 -0700

    a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,13 +1,8 @@
 #!/usr/bin/python3
-"""Script that fetches https://intranet.hbtn.io/status using requests."""
+""" Uses requests module -- yay"""
 import requests
 
 if __name__ == "__main__":
-    try:
-        r = requests.get('https://intranet.hbtn.io/status')
-        r.raise_for_status()  # Raises an HTTPError for bad responses
-        print("Body response:")
-        print("\t- type: {}".format(type(r.text)))
-        print("\t- content: {}".format(r.text))
-    except requests.exceptions.RequestException as e:
-        print("Error fetching the URL: {}".format(e))
+    response = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:\n\t- type: {}\n\t- content: {}"
+          .format(type(response.text), response.text))
