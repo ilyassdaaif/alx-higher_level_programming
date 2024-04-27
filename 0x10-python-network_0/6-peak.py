ee4463a63856b1f09da20e6db9afcf97b544ea57
ilyass <ilyass.df.01@gmail.com>
2024-04-27 13:37:17 -0700

    a

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,14 +1,29 @@
+#!/usr/bin/python3
+
+
 def find_peak(list_of_integers):
-    if not list_of_integers:
+
+    if list_of_integers is None or len(list_of_integers) == 0:
         return None
 
-    low, high = 0, len(list_of_integers) - 1
+    if len(list_of_integers) == 1:
+        return list_of_integers[0]
 
-    while low < high:
-        mid = (low + high) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            low = mid + 1  # Move right if the right element is greater
+    mid_idx = int(len(list_of_integers) / 2)
+
+    if mid_idx != len(list_of_integers) - 1:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx] and\
+           list_of_integers[mid_idx + 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
+    else:
+        if list_of_integers[mid_idx - 1] < list_of_integers[mid_idx]:
+            return list_of_integers[mid_idx]
         else:
-            high = mid  # Move left otherwise, including when equal
+            return list_of_integers[mid_idx - 1]
 
-    return list_of_integers[low] if list_of_integers else None
+    if list_of_integers[mid_idx - 1] > list_of_integers[mid_idx]:
+        a_list = list_of_integers[0:mid_idx]
+    else:
+        a_list = list_of_integers[mid_idx + 1:]
+
+    return find_peak(a_list)
