c737b33c8328653723908e920bdb9622f17c8d05
ilyass <ilyass.df.01@gmail.com>
2024-04-27 14:11:36 -0700

    a

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,8 +1,12 @@
 #!/usr/bin/python3
-""" Uses requests module -- yay"""
+"""
+Python script that fetches https://intranet.hbtn.io/status
+"""
 import requests
 
-if __name__ == "__main__":
-    response = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:\n\t- type: {}\n\t- content: {}"
-          .format(type(response.text), response.text))
+if __name__ == '__main__':
+
+    res = requests.get("https://intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(res.text)))
+    print("\t- content: {}".format(res.text))
