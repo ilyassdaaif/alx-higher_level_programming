66ca4a190dcdc40a102144921d3b0fee155a8023
ilyass <ilyass.df.01@gmail.com>
2024-04-27 13:52:42 -0700

    a

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,33 +1,23 @@
 #!/usr/bin/python3
-"""script for finding peak in list of ints, interview prep
-"""
+"""Defines a peak-finding algorithm."""
 
-"""
-    THOUGHT PROCESS
-        it is not sorted, so sorting would take n(log(n))
-            -> not worth sorting
-        looping through and keeping track of max (brute force)
-            -> O(n)
-
-        possibly looping from each end reducing to 1/2 run time
-            -> still O(n)
-        
-        best solution: Use a modified binary search to find a peak in O(log n) time.
-"""
 
 def find_peak(list_of_integers):
-    """Binary search implementation to find a peak efficiently.
-    """
-    if not list_of_integers:
+    """Return a peak in a list of unsorted integers."""
+    if list_of_integers == []:
         return None
-    
-    low, high = 0, len(list_of_integers) - 1
-    
-    while low < high:
-        mid = (low + high) // 2
-        if list_of_integers[mid] < list_of_integers[mid + 1]:
-            low = mid + 1  # The peak must be in the right half if the right neighbor is greater
-        else:
-            high = mid  # The peak is in the left half or at mid
 
-    return list_of_integers[low]  # low and high converge to the index of a peak
+    size = len(list_of_integers)
+    if size == 1:
+        return list_of_integers[0]
+    elif size == 2:
+        return max(list_of_integers)
+
+    mid = int(size / 2)
+    peak = list_of_integers[mid]
+    if peak > list_of_integers[mid - 1] and peak > list_of_integers[mid + 1]:
+        return peak
+    elif peak < list_of_integers[mid - 1]:
+        return find_peak(list_of_integers[:mid])
+    else:
+        return find_peak(list_of_integers[mid + 1:])
