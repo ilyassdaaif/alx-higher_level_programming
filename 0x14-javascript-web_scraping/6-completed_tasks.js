93d3b5ee7576407420509a298d9b399d48e560ad
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-05-21 22:13:23 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,26 +2,36 @@
 
 const request = require('request');
 
-if (process.argv.length < 4) {
-    console.error('Usage: node 5-request_store.js <URL> <file path>');
+if (process.argv.length < 3) {
+    console.error('Usage: node 6-completed_tasks.js <API URL>');
     process.exit(1);
 }
 
 const apiUrl = process.argv[2];
-const filePath = process.argv[3];
 
 request(apiUrl, function (error, response, body) {
-  if (error) {
-    console.error('Error:', error);
-  } else if (response.statusCode !== 200) {
-    console.error('Failed to get a valid response. Status code:', response.statusCode);
-  } else {
-    fs.writeFile(filePath, body, 'utf8', (err) => {
-        if (err) {
-            console.error('Error writing to file:', err);
-        } else {
-            console.log('File written successfully');
+    if (error) {
+        console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+        console.error('Failed to get a valid response. Status code:', response.statusCode);
+    } else {
+        try {
+            const todos = JSON.parse(body);
+            const completed = {};
+
+            todos.forEach((todo) => {
+                if (todo.completed) {
+                    if (completed[todo.userId] === undefined) {
+                        completed[todo.userId] = 1;
+                    } else {
+                        completed[todo.userId]++;
+                    }
+                }
+            });
+
+            console.log(completed);
+        } catch (parseError) {
+            console.error('Error parsing JSON:', parseError);
         }
-    });
-  }
+    }
 });
