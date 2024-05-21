676f70cfbe0a37c185c00f621e346abe14d4ea1b
ilyassdaaif <94968783+ilyassdaaif@users.noreply.github.com>
2024-05-21 22:25:07 +0100

    Update 6-completed_tasks.js

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,37 +1,16 @@
 #!/usr/bin/node
-
 const request = require('request');
-
-if (process.argv.length < 3) {
-    console.error('Usage: node 6-completed_tasks.js <API URL>');
-    process.exit(1);
-}
-
-const apiUrl = process.argv[2];
-
-request(apiUrl, function (error, response, body) {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error('Failed to get a valid response. Status code:', response.statusCode);
-    } else {
-        try {
-            const todos = JSON.parse(body);
-            const completed = {};
-
-            todos.forEach((todo) => {
-                if (todo.completed) {
-                    if (completed[todo.userId] === undefined) {
-                        completed[todo.userId] = 1;
-                    } else {
-                        completed[todo.userId]++;
-                    }
-                }
-            });
-
-            console.log(completed);
-        } catch (parseError) {
-            console.error('Error parsing JSON:', parseError);
-        }
-    }
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const todos = JSON.parse(body);
+    const completed = {};
+    todos.forEach((todo) => {
+      if (todo.completed && completed[todo.userId] === undefined) {
+        completed[todo.userId] = 1;
+      } else if (todo.completed) {
+        completed[todo.userId] += 1;
+      }
+    });
+    console.log(completed);
+  }
 });
