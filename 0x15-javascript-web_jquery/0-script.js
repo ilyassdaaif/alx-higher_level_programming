e1b267e713eba0e8bb80c143002910854defd3dd
ilyassdaaif <ilyassdaaif@gmail.com>
2024-05-29 13:08:06 -0700

    javascript web jquery

---
+++ 0x15-javascript-web_jquery/0-script.js
@@ -0,0 +1,5 @@
+// Use document.querySelector to select the <header> element
+const headerElement = document.querySelector('header');
+
+// Change the text color of the <header> element to red
+headerElement.style.color = '#FF0000';
