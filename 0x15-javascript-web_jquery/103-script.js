55c1304f82b415a0419a26bd62458eeb46f8e00d
ilyassdaaif <ilyassdaaif@gmail.com>
2024-06-03 17:56:15 -0700

    b

---
+++ 0x15-javascript-web_jquery/103-script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  function fetchTranslation() {
+    const langCode = $('#language_code').val();
+    const url = `https://www.fourtonfish.com/hellosalut/hello/?lang=${langCode}`;
+    $.get(url, function(data) {
+      $('#hello').text(data.hello);
+    });
+  }
+
+  $('#btn_translate').click(fetchTranslation);
+
+  $('#language_code').keypress(function(event) {
+    if (event.which === 13) {
+      fetchTranslation();
+    }
+  });
+});
